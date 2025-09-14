4f01240c23babd272a5631ab61fb381521e6a387
園田和裕 <sonodakazuhiro@sonodakazuhiros-MacBook-Air.local>
2025-09-14 22:07:41 +0900

    3 commit

--- mental_predict.py
+++ mental_predict.py
@@ -3,24 +3,28 @@ import pandas as pd
 import numpy as np
 import joblib
 
-def predict_future(days_ahead, csv_path="mentalwave_input_data.csv"):
-    model = load_model("mental_model.h5")
-    scaler = joblib.load("scaler.pkl")
+# モデルとスケーラーは関数外でロード
+model = load_model("mental_model.h5")
+scaler = joblib.load("scaler.pkl")
 
-    df = pd.read_csv(csv_path)
+def predict_future(days_ahead, csv_path="mentalwave_input_data.csv"):
+    df = pd.read_csv(csv_path).tail(20)
     X = df.iloc[:, :11].values
     X = scaler.transform(X)
 
     time_steps = 5
     history_window = list(X[-time_steps:])
 
-    prediction = None
+    predictions = []
     for _ in range(days_ahead):
         x_input = np.array(history_window[-time_steps:])
-        x_input = np.expand_dims(x_input, axis=0)
-        prediction = model.predict(x_input, verbose=0)[0][0]
+        x_input = np.expand_dims(x_input, axis=0)  # shape (1, time_steps, features)
+
+        pred = model.predict(x_input, verbose=0)[0][0]
+        predictions.append(pred)
+
         fake_features = history_window[-1].copy()
-        fake_features[0] = prediction  
+        fake_features[0] = pred  
         history_window.append(fake_features)
 
-    return prediction
+    return predictions
